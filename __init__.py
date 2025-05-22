182b27495d91150595edf6f5170aabbafb351efe
BLANCHARD, Theau <theau.blanchard@gehealthcare.com>
2025-05-22 13:55:35 +0200

    Move files to the correct location

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .geodesic_distance import *
-from .cometric import *
-from .utils import *
-from .samplers import *
-from .projected_geodesic_distance import ProjectedGeodesicDistance
---
+++ src/geodesic_toolbox/__init__.py
@@ -0,0 +1,4 @@
+from .solvers import *
+from .cometric import *
+from .utils import *
+from .samplers import *
